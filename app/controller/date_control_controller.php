e36f923029bea0163abeda36163f282a2ad53181
henrygotmojo <henrygotmojo@gmail.com>
2023-07-03 15:20:45 +0800

    show message fields of multiple languages (when necessary)

--- app/controller/date_control_controller.php
+++ app/controller/date_control_controller.php
@@ -3,19 +3,16 @@ F::redirect('auth&callback='.base64_encode($_SERVER['REQUEST_URI']), !Auth::user
 F::error('Forbidden', !Auth::userInRole('SUPER,ADMIN'));
 
 
-// message fields in different language
-$localeAll = class_exists('I18N') ? I18N::localeAll() : array('en');
-$localeCount = count($localeAll);
-$remarkFields = array();
-foreach ( $localeAll as $lang ) {
-	$fieldName = ( $lang == 'en' ) ? 'remark' : str_replace('-', '_', "remark__{$lang}");
-	$remarkFields[$fieldName] = array(
-		'format' => 'textarea',
-		'style' => 'height: 66px',
-		'label' => ( $fieldName == 'remark' ) ? 'Message' : false,
-		'inline-label' => ( $localeCount > 1 ) ? "<div class='text-uppercase' style='width: 2.5rem'>{$lang}</div>" : false,
+// message fields in different languages
+$remarkFields = array_merge(...array_map(function($lang){
+	$suffix = ( $lang == 'en' ) ? '' : ('__'.str_replace('-', '_', $lang));
+	return array(
+		'tmp.beforeMessage'.$suffix => array('label' => ( $lang == 'en' ) ? 'Messages' : '', 'inline-label' => '<small class="d-block" style="width: 40px;"><b>BEFORE</b></small>', 'pre-help' => ( $lang == 'en' ) ? '' : "<b class='text-info fa-sm'>{$lang}</b>" ),
+		'tmp.afterMessage'.$suffix => array('label' => false, 'inline-label' => '<small class="d-block" style="width: 40px;"><b>AFTER</b></small>'),
+		'tmp.nowMessage'.$suffix => array('label' => false, 'inline-label' => '<small class="d-block" style="width: 40px;"><b>NOW</b></small>'),
+		'remark'.$suffix => array('format' => 'hidden', 'label' => false),
 	);
-}
+}, class_exists('I18N') ? I18N::localeAll() : array('en')));
 
 
 // config
@@ -23,6 +20,7 @@ $scaffold = array_merge([
 	'beanType' => 'enum',
 	'editMode' => 'inline',
 	'allowDelete' => Auth::userInRole('SUPER'),
+	'allowSort' => false,
 	'layoutPath' => F::appPath('view/global/layout.php'),
 	'listFilter' => array('type = ?', ['DATE_CONTROL']),
 	'listOrder' => 'ORDER BY `key` ',
@@ -30,19 +28,15 @@ $scaffold = array_merge([
 		'id' => '60',
 		'key|type' => '15%',
 		'tmp.startDatetime|tmp.endDatetime|value' => '20%',
-		'tmp.beforeMessage|tmp.afterMessage|tmp.nowMessage|remark' => '40%',
+		implode('|', array_keys($remarkFields)) => '40%',
 	),
-	'fieldConfig' => array(
+	'fieldConfig' => array_merge([
 		'key' => array('readonly' => !Auth::userInRole('SUPER')),
 		'type' => array('label' => false, 'value' => 'DATE_CONTROL', 'readonly' => true),
 		'value' => array('format' => 'hidden', 'label' => false),
-		'remark' => array('format' => 'hidden', 'label' => false),
 		'tmp.startDatetime' => array('label' => 'Start', 'format' => 'datetime', 'inline-label' => '<small class="d-block" style="width: 30px;"><b>START</b></small>'),
 		'tmp.endDatetime' => array('label' => 'End', 'format' => 'datetime', 'inline-label' => '<small class="d-block" style="width: 30px;"><b>END</b></small>'),
-		'tmp.beforeMessage' => array('label' => 'Messages', 'inline-label' => '<small class="d-block" style="width: 40px;"><b>BEFORE</b></small>'),
-		'tmp.afterMessage' => array('label' => false, 'inline-label' => '<small class="d-block" style="width: 40px;"><b>AFTER</b></small>'),
-		'tmp.nowMessage' => array('label' => false, 'inline-label' => '<small class="d-block" style="width: 40px;"><b>NOW</b></small>'),
-	),
+	], $remarkFields),
 	'scriptPath' => array(
 		'row' => F::appPath('view/date_control/row.php'),
 //		'inline_edit' => F::appPath('view/date_control/inline_edit.php'),
