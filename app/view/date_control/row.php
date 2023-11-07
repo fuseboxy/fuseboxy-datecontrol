b18be1d3004a8bb47299711e06f36a8d91a4d4ba
henrygotmojo <henrygotmojo@gmail.com>
2023-11-07 11:56:24 +0800

    fix bug of displaying dates & messages in disabled row

--- app/view/date_control/row.php
+++ app/view/date_control/row.php
@@ -13,10 +13,6 @@
 	</io>
 </fusedoc>
 */
-// multi-language (when necessary)
-$localeAll = class_exists('I18N') ? I18N::localeAll() : ['en'];
-
-
 // capture original output
 ob_start();
 include F::appPath('view/scaffold/row.php');
@@ -24,25 +20,31 @@ $doc = Util::phpQuery(ob_get_clean());
 
 
 // start & end
-foreach ( ['start','end'] as $periodType ) :
-	$tmpValue = DateControl::get($bean->key, $periodType);
-	F::error(DateControl::error(), $tmpValue === false);
-	$doc->find('div.col-tmp-'.$periodType.'Datetime')->html($tmpValue);
-endforeach;
+// ===> parse from raw data instead of using DateControl methods
+// ===> because DateControl methods can only obtain non-disabled records
+$tmpPeriod = explode('|', $bean->value);
+$doc->find('div.col-tmp-startDatetime')->html($tmpPeriod[0] ?? '');
+$doc->find('div.col-tmp-endDatetime')->html($tmpPeriod[1] ?? '');
 
 
-// messages
+// messages (could be multi-languages)
+// ===> parse from raw data instead of using DateControl methods
+// ===> because DateControl methods can only obtain non-disabled records
+$localeAll = class_exists('I18N') ? I18N::localeAll() : ['en'];
 foreach ( $localeAll as $lang ) :
-	$langSuffix = ( $lang == 'en' ) ? '' : ('__'.str_replace('-', '_', $lang));
+	// determine field suffix
+	if ( $lang == 'en' ) $langSuffix = '';
+	else $langSuffix = '__'.str_replace('-', '_', $lang);
+	// display message
+	$remarkField = 'remark'.$langSuffix;
+	$tmpMessage = explode("\n", $bean->{$remarkField} ?? $bean->remark);
+	$doc->find('div.col-tmp-beforeMessage'.$langSuffix)->html($tmpMessage[0] ?? '');
+	$doc->find('div.col-tmp-afterMessage'.$langSuffix)->html($tmpMessage[1] ?? '');
+	$doc->find('div.col-tmp-nowMessage'.$langSuffix)->html($tmpMessage[2] ?? '');
+	// display badge & adjust style
 	foreach ( ['before','after','now'] as $msgType ) :
-		$msg = $doc->find('div.col-tmp-'.$msgType.'Message'.$langSuffix);
-		$msg->removeClass('small')->removeClass('text-muted');
-		if ( class_exists('I18N') ) I18N::set($lang);
-		$tmpMsg = DateControl::message($bean->key, $msgType);
-		F::error(DateControl::error(), $tmpMsg === false);
-		$msg->html($tmpMsg);
-		if ( class_exists('I18N') ) I18N::reset();
-		$msg->prepend('<span class="badge badge-light b-1 small" style="width: 50px;">'.strtoupper($msgType).'</span> ');
+		$badge = '<span class="badge badge-light b-1 small mr-1" style="width: 50px;">'.strtoupper($msgType).'</span>';
+		$doc->find('div.col-tmp-'.$msgType.'Message'.$langSuffix)->prepend($badge)->removeClass('text-muted')->removeClass('small');
 	endforeach;
 endforeach;
 
